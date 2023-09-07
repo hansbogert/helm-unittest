a66a61788700d748da4899c816fededa4d56ff21
Jonathon McNabb <jonathon@lunaroutpost.com>
2023-09-07 13:49:01 -0600

    Implement support for documentSelector

--- pkg/unittest/test_job.go
+++ pkg/unittest/test_job.go
@@ -2,6 +2,7 @@ package unittest
 
 import (
 	"fmt"
+	"errors"
 	"io"
 	"os"
 	"path"
@@ -121,6 +122,11 @@ func (s *orderedSnapshotComparer) CompareToSnapshot(content interface{}) *snapsh
 	return s.cache.Compare(s.test, s.counter, content)
 }
 
+type DocumentSelector struct {
+	Path  string
+	Value string
+}
+
 // TestJob definition of a test, including values and assertions
 type TestJob struct {
 	Name          string `yaml:"it"`
@@ -129,6 +135,7 @@ type TestJob struct {
 	Template      string
 	Templates     []string
 	DocumentIndex *int `yaml:"documentIndex"`
+	DocumentSelector *DocumentSelector  `yaml:"documentSelector"`
 	Release       struct {
 		Name      string
 		Namespace string
@@ -182,14 +189,14 @@ func (t *TestJob) RunV3(
 		// Continue to enable matching error via failedTemplate assert
 	}
 
-	// Setup Assertion Templates based on the chartname and outputOfFiles
-	t.polishAssertionsTemplate(targetChart.Name(), outputOfFiles)
-
 	manifestsOfFiles, err := t.parseManifestsFromOutputOfFiles(targetChart.Name(), outputOfFiles)
 	if err != nil {
 		result.ExecError = err
 		return result
 	}
+
+	// Setup Assertion Templates based on the chartname and outputOfFiles
+	t.polishAssertionsTemplate(targetChart.Name(), outputOfFiles, manifestsOfFiles)
 
 	snapshotComparer := &orderedSnapshotComparer{cache: cache, test: t.Name}
 	result.Passed, result.AssertsResult = t.runAssertions(
@@ -203,6 +210,28 @@ func (t *TestJob) RunV3(
 	result.Duration = time.Since(startTestRun)
 	return result
 }
+
+func findDocumentIndex(manifests map[string][]common.K8sManifest, selector DocumentSelector) (int, error) {
+    for _, fileManifests := range manifests {
+        for idx, doc := range fileManifests {
+            manifestValues, err := valueutils.GetValueOfSetPath(doc, selector.Path)
+            if err != nil {
+                continue
+            }
+
+            for _, v := range manifestValues {
+                if valueStr, ok := v.(string); ok {
+                    if valueStr == selector.Value {
+                        return idx, nil
+                    }
+                }
+            }
+        }
+    }
+
+    return -1, errors.New("document not found")
+}
+
 
 // liberally borrows from helm-template
 func (t *TestJob) getUserValues() ([]byte, error) {
@@ -447,11 +476,23 @@ func (t *TestJob) determineRenderSuccess() {
 }
 
 // add prefix to Assertion.Template
-func (t *TestJob) polishAssertionsTemplate(targetChartName string, outputOfFiles map[string]string) {
+func (t *TestJob) polishAssertionsTemplate(targetChartName string, outputOfFiles map[string]string, manifestOfFiles map[string][]common.K8sManifest) {
 	if t.chartRoute == "" {
 		t.chartRoute = targetChartName
 	}
 
+	if t.DocumentSelector != nil {
+		idx, err := findDocumentIndex(manifestOfFiles, *t.DocumentSelector)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			// Update the DocumentIndex if the found idx is not -1
+			if idx != -1 {
+				t.DocumentIndex = &idx
+			}
+		}
+	}
+	
 	for _, assertion := range t.Assertions {
 		prefixedChartsNameFiles := false
 		templatesToAssert := make([]string, 0)
